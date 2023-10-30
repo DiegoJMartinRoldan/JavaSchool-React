4eb4f69fbf105b2b39af89d78683363026c894dd
DiegoJMartinRoldan <diegojmr1999@gmail.com>
2023-10-30 21:32:54 +0100

    New React Project

---
+++ JavaSchool-FinalTask2-React-Diego/src/App.jsx
@@ -0,0 +1,17 @@
+export function App (){
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/kikobeats" alt="Random Avatar" />
+                <div>
+                    <strong>Random</strong>
+                    <span>@random</span>
+                </div>
+            </header>
+            <aside>
+                <button>Follow</button>
+            </aside>
+        </article>
+    )
+
+}
